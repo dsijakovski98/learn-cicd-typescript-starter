e21b4d188cb137b48a38ce331227a7c632424102
dsijakovski98 <dshijakovski@yahoo.com>
2026-04-30 13:23:36 +0200

    Add prettier CI

--- src/tests/api-auth.test.ts
+++ src/tests/api-auth.test.ts
@@ -2,25 +2,25 @@ import type { IncomingHttpHeaders } from "http";
 import { getAPIKey } from "../api/auth";
 import { describe, expect, test } from "vitest";
 
-describe('API auth - API key is fetched correctly', () => {
-    test('Authorization header missing handled', () => {
-        expect(getAPIKey({})).toBeNull()
-    })
+describe("API auth - API key is fetched correctly", () => {
+  test("Authorization header missing handled", () => {
+    expect(getAPIKey({})).toBeNull();
+  });
 
-    test('Authorization header missing "ApiKey" protocol', () => {
-        const headers: IncomingHttpHeaders = {
-            authorization: 'Bearer API_KEY_HERE'
-        }
+  test('Authorization header missing "ApiKey" protocol', () => {
+    const headers: IncomingHttpHeaders = {
+      authorization: "Bearer API_KEY_HERE",
+    };
 
-        expect(getAPIKey(headers)).toBeNull()
-    })
+    expect(getAPIKey(headers)).toBeNull();
+  });
 
-    test('API key correctly extracted', () => {
-        const apiKey = 'API_KEY_HERE'
-        const headers: IncomingHttpHeaders = {
-            authorization: `ApiKey ${apiKey}`
-        }
+  test("API key correctly extracted", () => {
+    const apiKey = "API_KEY_HERE";
+    const headers: IncomingHttpHeaders = {
+      authorization: `ApiKey ${apiKey}`,
+    };
 
-        expect(getAPIKey(headers)).toBe(apiKey)
-    })
-})
+    expect(getAPIKey(headers)).toBe(apiKey);
+  });
+});
